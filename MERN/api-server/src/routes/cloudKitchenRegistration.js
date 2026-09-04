4f9e94a99f024d1050347af1b75b84240f493e12
meesamriyaz-cyber <meesamriyaz@gmail.com>
2026-09-04 10:00:29 +0530

    Remove unused temporary registration route

--- MERN/api-server/src/routes/cloudKitchenRegistration.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { registerCloudKitchen } from '../controllers/cloudKitchenRegistrationController.js';
-
-const router = Router();
-
-router.post('/cloud-kitchen/register', registerCloudKitchen);
-
-export default router;
